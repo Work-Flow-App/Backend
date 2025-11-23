d72b9a452602d777a0c753365ba7f2d2af7ad6fc
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2025-11-23 18:22:54 +0100

    chore: make field required in clients

--- src/main/java/com/workflow/dto/client/ClientUpdateRequest.java
+++ src/main/java/com/workflow/dto/client/ClientUpdateRequest.java
@@ -1,5 +1,7 @@
 package com.workflow.dto.client;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Data
@@ -7,7 +9,10 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class ClientUpdateRequest {
+    @NotBlank(message = "Client name is required")
+    @Size(min = 2, max = 150, message = "Client name must be between 2 and 150 characters")
     private String name;
+
     private String email;
     private String telephone;
     private String mobile;

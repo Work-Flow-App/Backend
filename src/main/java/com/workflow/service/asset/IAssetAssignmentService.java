e283d4e39a5c2df202555e094296a590d8db4bb4
Ta-SeenJunaid <taseen.junaid@gmail.com>
2025-12-01 10:15:09 +0100

    feat(asset): implement asset assignment

---
+++ src/main/java/com/workflow/service/asset/IAssetAssignmentService.java
@@ -0,0 +1,14 @@
+package com.workflow.service.asset;
+
+import com.workflow.dto.asset.*;
+import java.util.List;
+
+public interface IAssetAssignmentService {
+    AssetAssignmentResponse assignAsset(AssetAssignmentCreateRequest request, Long companyId);
+
+    AssetAssignmentResponse returnAsset(AssetAssignmentReturnRequest request, Long companyId);
+
+    List<AssetAssignmentResponse> getAssignmentHistory(Long assetId, Long companyId);
+
+    List<AssetAssignmentResponse> getAssignedAssetsForJob(Long jobId, Long companyId, boolean onlyActive);
+}

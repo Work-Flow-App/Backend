d7ccacb8dc93e5637467407e1e4469c000b84d08
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2026-04-19 09:46:16 +0200

    feat: company currency setup

---
+++ src/main/java/com/workflow/common/constant/Currency.java
@@ -0,0 +1,20 @@
+package com.workflow.common.constant;
+
+public enum Currency {
+    GBP,
+    EUR,
+    USD,
+    CAD,
+    AUD,
+    NZD,
+    CHF,
+    JPY,
+    SEK,
+    NOK,
+    DKK,
+    SGD,
+    AED,
+    SAR,
+    INR,
+    ZAR
+}

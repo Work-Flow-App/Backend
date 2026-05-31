1e186b61f6e0d6984d00aa90b551a0870376660c
Ta-Seen Junaid <taseen.junaid@gmail.com>
2026-05-31 19:24:39 +0200

    fix(time): convert to UTC time (#215)

--- src/main/java/com/workflow/Application.java
+++ src/main/java/com/workflow/Application.java
@@ -1,15 +1,25 @@
 package com.workflow;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class Application {
+
+	@PostConstruct
+	public void init() {
+		// Force the JVM default timezone to UTC
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
